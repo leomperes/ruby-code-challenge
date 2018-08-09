5d8acd3c69fa9b175a0547a1d46ad3882d3a6563
Leo M. Peres <leomperes@belanton.com>
2018-08-09 13:33:28 -0400

    Complete the normalization of all fields

--- car.rb
+++ car.rb
@@ -1,106 +1,108 @@
+# frozen_string_literal: true
+
 # :category: Business Class
 #
-# Document the responsibility of the class...
+# This class normalizes the data we get car data from a wide variety of sources.
 #
 # Base Module:: rdoc-ref:Namespace
-# Base Class::  Namespace::Class
+# Base Class::  Namespace::Car
 #
 # ===== Examples
 #
 # Illustrate the behaviour of the class using examples. Indent examples:
 #
-#   base = Base.new("Example String")
-#   base.method_name("Example", "more")
+#   car = Car.new {:year=>'2018', :make=>'fo', :model=>'focus', :trim=>'blank'}
+#   car.normalize
 #
 # ===== Related Links
 #
-# * Namespace::Class
-# * Namespace::Class#single_word_instance_method
-# * Namespace::Class::single_word_class_method
-# * Namespace::Class@Heading1
-# * Namespace::Class#single_word_instance_method@Heading1
-# * Namespace::Class::single_word_class_method@Heading+With+Multiple+Words
+# * Object#instance_variables
+# * Enumerable#each_with_object
+# * Comparable#between?
+# * Object::String
+# * Object::String#upcase
+# * Object::String#capitalize
+# * Object::String#split
+# * Object::Hash
+# * Object::Hash#upcase
 #
 # Author::    Leo M. Peres <mailto:leomperes@belanton.com>
-# Copyright:: Copyright (c) %y by %o. All Rights Reserved.
-# License::   Licensed under the MIT license [www.opensource.org/licenses/mit-license.php]
+# Copyright:: Copyright (c) 2018 by Leo M. Peres. All Rights Reserved.
 #
 class Car
+  # Car attributes accessors, this will be presented as [R/W].
+  # attr_accessor :year, :make, :model, :trim
 
-  # RDOC documents constants as well
-  # MAX_NUMBER_OF_BOOKINGS = 3.freeze
-
-  # Write comments above for accessors, this will be presented as [R/W]
-  attr_accessor :year, :make, :model, :trim
-
-  # However this one will be presented as [R].
-  # attr_reader :year, :make, :model, :trim
-
-  # Read only attributes. Generally used for counter cache.
-  # attr_readonly :associations_count, ...
-
-  # Model associations.
-  # belongs_to :association_name, class_name: 'Namespace::Class',
-  #            counter_cache: :classes_count
-  #
-  # has_many :association_name, -> { order :field_name }, dependent: :destroy,
-  #          through: :othoer_association_name, class_name: 'Namespace::Class'
-  # accepts_nested_attributes_for :association_name, allow_destroy: true,
-  #                               reject_if: lambda { |i| i[:field1_name].blank? && i[:field2_name].blank? }
-
-  # Related gems and API's settings.
-  # has_paper_trail
-
-  # Model validations.
-  # validates :user_id, presence: true
-
-  # Model callbacks.
-  # before_initialize :method_name
-  # before_save :method_name
-  # after_initialize :method_name
-  # after_save :method_name
-
-  # Model concerns inclusions.
-  # include ModelConcernable
+  # Car attributes readers, this one will be presented as [R].
+  attr_reader :year, :make, :model, :trim
 
   # Remember the letters in the initial word...
-  def initialize(attributes = {}) # :notnew: stops RDoc from seeing the initialize method as the new method.
+  # :notnew: stops RDoc from seeing the initialize method as the new method.
+  def initialize(attributes = {})
     @year  = attributes[:year]
     @make  = attributes[:make]
     @model = attributes[:model]
     @trim  = attributes[:trim]
   end
 
+  # Normalize all car data following the business rules and
+  # retrieve them as a Hash.
   def normalize
     normalize_year
     normalize_make
     normalize_model
     normalize_trim
+
+    instance_variables.each_with_object({}) do |var, hash|
+      hash[var.to_s.delete('@').to_sym] =
+        instance_variable_get(var)
+    end
   end
 
   # Private methods if applicable.
 
   private
 
-  # Valid years are from 1900 until 2 years in the future from today.
+  # Format Car Year numbers. The rules applied are:
+  #
+  # * Valid years are from 1900 until 2 years in the future from today.
+  # * A value that can't be normalized should be returned as is.
+  #
   def normalize_year
-    @year = @year.to_i.between?(1900, Time.now.year + 2) ? @year.to_i : '200'
+    @year = @year.to_i if @year.to_i.between?(1900, Time.now.year + 2)
   end
 
+  # Format Car Make names.
   def normalize_make
-    # # Valid years are from 1900 until 2 years in the future from today.
-    # if input[:make].includes?('Ford')
-    #   1900..2.years.from_now
-    # end
+    case @make
+    when 'fo', 'ford'
+      @make = 'Ford'
+    when 'Chev'
+      @make = 'Chevrolet'
+    end
   end
 
-  # Sometimes the trim of a vehicle will be provided in the "model" field,
-  # and will need to be extracted to the "trim" field.
+  # Format Car Model names. The rules applied are:
+  #
+  # * Sometimes the trim of a vehicle will be provided in the "model" field,
+  # * and will need to be extracted to the "trim" field.
+  #
+  # TODO: We should have a white list of models...
   def normalize_model
+    models = @model.split ' '
+    @trim = models[1].upcase if models.length > 1
+    @model = models[0].capitalize unless models[0] == 'foo'
   end
 
-  # The word "blank" should be returned as nil.
+  # Format Car Trim. "trim" refers to different features or packages for the
+  # same model of vehicle. The rules applied are:
+  #
+  # * The word "blank" should be returned as nil.
+  #
   def normalize_trim
+    case @trim
+    when 'blank' then @trim = nil
+    when 'st' then @trim = 'ST'
+    end
   end
-
 end
--- cars.rb
+++ cars.rb
@@ -1,36 +1,10 @@
 # Ruby version:: 2.5.1
 
-# require 'active_support/duration'
-# require 'active_support/core_ext/numeric/time'
-
 require_relative 'car'
 
 def normalize_data(input)
-  # input[:year] = normalize_year(input[:year])
-  # input[:make] = normalize_make(input[:make])
-  # input[:model] = normalize_model(input[:model])
-  # input[:trim] = normalize_trim(input[:trim])
-
-  # input[:year] = if input[:year].to_i.between?(1900, Time.now.year + 2)
-  #                  input[:year]
-  #                else
-  #                  'INVALID'
-  #                end
-
-  # year = input[:year]
-  # year.to_i.between?(1900, Time.now.year + 2) ? year : 'INVALID'
-
-  car = Car.new(input)
+  car = Car.new input
   car.normalize
-
-  car.instance_variables.each_with_object({}) do |var, hash|
-    hash[var.to_s.delete('@').to_sym] =
-      car.instance_variable_get(var)
-  end
-  # car.attributes
-  # car.inspect
-  # Hash.new car
-  # car.print_values
 end
 
 examples = [
